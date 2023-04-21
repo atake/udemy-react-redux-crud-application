cf3dd4ec9b1e84d5cb65a4266370035cb8a338bc
HIromichi Takenaka <atake@usernoMacBook-Pro.local>
2023-04-21 17:04:51 +0900

    create my React boilerplate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello, world React</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+
+
+class App extends Component{
+  render(){
+    return  <div><h1>Hello,World</h1></div>;
+  }
 }
 
 export default App;
